d749cc0706f1f85b2ed26370e0b48825e82b6fef
JapoDeveloper <julio_master_55@hotmail.com>
2019-06-20 11:47:13 -0400

    Initial commit

---
+++ src/main/java/co/japo/demo/buying_analytics/BuyingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package co.japo.demo.buying_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyingAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyingAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/japo/demo/buying_analytics/BuyingAnalyticsApplicationTests.java
@@ -0,0 +1,16 @@
+package co.japo.demo.buying_analytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuyingAnalyticsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
